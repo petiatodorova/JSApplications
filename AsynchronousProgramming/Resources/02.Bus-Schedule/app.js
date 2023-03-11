91f2b1eb2febff48ccf47319575fe9de99471863
Petya Todorova <invisioncl@gmail.com>
2023-03-11 23:49:13 +0200

    JS Apps Async

--- AsynchronousProgramming/Resources/02.Bus-Schedule/app.js
+++ AsynchronousProgramming/Resources/02.Bus-Schedule/app.js
@@ -1,11 +1,33 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    const label = document.querySelector('#info span');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+
+    let stop = {
+        next: 'depot'
+    };
+
+    async function depart() {
+        departBtn.disabled = true;
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`;
+        const res = await fetch(url);
+
+        if (res.status !== 200) {
+            label.textContent = `Error`;
+            departBtn.disabled = true;
+            arriveBtn.disabled = true;
+        }
+
+        stop = await res.json();
+        label.textContent = `Next stop ${stop.name}`;
+        arriveBtn.disabled = false;
     }
 
-    function arrive() {
-        console.log('Arrive TODO...');
+    async function arrive() {
+        label.textContent = `Arriving at ${stop.name}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
